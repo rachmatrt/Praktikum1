cfa4eeccc34728d5703f851764ed80b498b0b872
rachmatrt <rachmatrt05@gmail.com>
2018-04-11 19:04:46 +0700

    First

---
+++ praktikum1/index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Praktikum</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-theme.css" rel="stylesheet">
+    <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  	<!--Start header-->
+<div class="container-fluid">
+ <div class="row">
+    <div class="col-xs-12 col-md-12" style="border: 1px solid; background-color: #88fc5a;height: 8%;">
+
+    	<a href="#Footer" class="navbar-brand" id="head" >Header</a>
+    	    	
+    </div>
+ </div>
+</div> 
+    <!--End header-->
+ 
+    <!--Start menu-->
+ <div class="container-fluid" >
+ <div class="row"">
+    <div class="col-xs-2 col-md-2 sidebar" style="border: 1px solid;height: 900px" id="head">
+    	
+    	<ul class="nav nav-sidebar" align="center">
+    		<li><a href="#content1" >Menu1 </a></li>
+    		<li><a href="#content2" >Menu2 </a></li>
+    		<li><a href="#content3" >Menu3 </a></li>
+    	</ul>	
+    	
+    </div>
+    <!--End menu-->
+    <!--Start content-->
+    <div class="col-xs-10 col-md-10" style="border: 1px solid;">
+    	<div style="height: 900px;border: 1px solid"><!--content1-->
+    		
+    		<h1 align="center" id="content1">Content1</h1>	
+	
+		</div >
+		<div style="height: 900px;border: 1px solid"><!--content2-->
+    		
+    		<h1 align="center" id="content2">Content2</h1>	
+	
+		</div >
+    	<div style="height: 900px;border: 1px solid"><!--content2-->
+    		
+    		<h1 align="center" id="content3">Content3</h1>	
+		
+		</div >
+
+    </div>	
+ </div>
+    <!--End content--> 
+
+</div>
+    <!--StartFooter-->
+ <div class="row">
+    <div class="col-xs-12 col-md-12" style="border: 1px solid;height: 8%; background-color: #88fc5a">
+    	
+    		<a href="#head" class="navbar-brand" id="footer" >Footer</a>
+    	
+    </div>
+ </div>
+<!--EndFooter-->
+   
+  </body>
+   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+</html>   
